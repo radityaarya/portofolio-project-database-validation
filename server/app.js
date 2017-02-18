66de007aa37e47eff9d2a8bd2f56e6dbc72e2e42
Raditya <arya.raditya26@gmail.com>
2017-02-18 15:30:24 +0700

    app.js config

--- server/app.js
+++ server/app.js
@@ -4,9 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors')
+require('dotenv').config()
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var events = require('.routes/events.route')
 
 var app = express();
 
@@ -21,9 +24,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/', index);
 app.use('/users', users);
+app.user('/events', events);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
